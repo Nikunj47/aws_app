1414035c944027369b0bc171e0c051186d60b761
Nikunj Chhabra <nikunj.chhabra@yahoo.com>
2024-07-19 00:35:20 -0400

    Cognito

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import './styles/styles.css';
-import Sidebar from './components/Sidebar';
-import Form from './components/PostForm.js';
-import Preview from './components/Preview.js'
-
+// import './styles/styles.css';
+// import Sidebar from './components/Sidebar';
+// import Form from './components/PostForm.js';
+// import Preview from './components/Preview.js'
+import { Amplify } from 'aws-amplify'
+import awsconfig from './aws-exports';
+import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+Amplify.configure(awsconfig)
 const App = () => {
   return (
-      <div className="app">
-
-        <Sidebar />
-        <div className="content">
-          <Form />
-        </div>
-        <div className="Preview">
-          <Preview />
-          </div>
-      </div>
+      <Authenticator>
+                {({ signOut, user }) => (
+                  <main>
+                        <h1>Hello {user.username}</h1>
+                        <button onClick={signOut}>Sign out</button>
+                      </main>
+                )}
+              </Authenticator>
   );
 };
 
-export default App;
+export default withAuthenticator(App);
