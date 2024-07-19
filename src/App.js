e3b45d72ed0f74f3994534fcd4db38579ab74783
Nikunj Chhabra <nikunj.chhabra@yahoo.com>
2024-07-19 19:52:42 -0400

    Button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { graphqlOperation } from 'aws-amplify';
+import Button from '@mui/material/Button';
 import React from 'react';
 // import './styles/styles.css';
 // import Sidebar from './components/Sidebar';
@@ -9,12 +11,22 @@ import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 Amplify.configure(awsconfig)
 const App = () => {
+
+    // const fetchData = async () => {
+    //     try {
+    //         const res = await API.graphql(graphqlOperation(listPosts));
+    //         console.log(res);
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
   return (
       <Authenticator>
                 {({ signOut, user }) => (
                   <main>
                         <h1>Hello {user.username}</h1>
-                        <button onClick={signOut}>Sign out</button>
+                        <Button onClick={signOut} variant={"outlined"}>Sign out</Button>
+                      {/*<Button variant="contained">Hello world</Button>;*/}
                       </main>
                 )}
               </Authenticator>
